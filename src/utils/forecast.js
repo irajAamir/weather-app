c8448db22c2f2891c50536492b6ac05683e0f9e7
irajAamir <66646807+irajAamir@users.noreply.github.com>
2020-07-07 19:23:43 +0500

    humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast=(latitude,longitude, callback)=>{
             callback('Unable to find location.', undefined)
 
         }else{
-            callback(undefined,body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature+" degrees out & feels like " + body.current.feelslike+".")
+            callback(undefined,body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature+" degrees out & feels like " + body.current.feelslike+". Humidity is " + body.current.humidity+ ".")
 
         }
     })
